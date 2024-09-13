5afa112d64414eea0a778e14b5aea56d66536ef5
PabloSFlores <i20213tn013@utez.edu.mx>
2024-09-12 18:57:30 -0600

    primer commit

---
+++ sw.js
@@ -0,0 +1,25 @@
+console.log('SW Hola mundo !!');
+
+self.addEventListener('install', (event) => {
+    console.log('SW: Instalado');
+});
+
+self.addEventListener('fetch', (event) => {
+    console.log(event.request.url);
+    console.log("Estilos cambiados");
+
+    if (event.request.url.includes('style.css')) {
+        const respuesta = new Response(
+            `body{
+                color: white;
+                background-color: #000;
+            }`,
+            {
+                headers: {
+                    'Content-Type': 'text/css'
+                }
+            }
+        );
+        event.respondWith(respuesta);
+    }
+})
